c291b4140dd4d82d00b597d1ac67471274d3c25e
wiwindeveloper <n.wiwinsavitri@gmail.com>
2021-12-21 09:25:36 +0800

    update port env file

--- index.js
+++ index.js
@@ -102,6 +102,6 @@ app.get('/api/tronwallet', (req, res) => {
 // app.listen(3000,() =>{
 //   console.log('Server started on port 3000...');
 // });
-app.listen(80,() =>{
-  console.log('Server started on port 80...');
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
